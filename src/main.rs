2eca11f36a97792404c47e33751befece1b1f14f
Tim Steeman <tmdev82@gmail.com>
2022-10-12 22:33:54 +0200

    set up basic yew app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+        <div>
+            <h1>{"Yew image app"}</h1>
+        </div>
+        </>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<App>();
 }
